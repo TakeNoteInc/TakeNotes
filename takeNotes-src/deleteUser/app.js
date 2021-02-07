c9580cd40dc02a3de31dc00670dc1cd9be546050
KrishnaSolo <krishna.solanki@mail.utoronto.ca>
2021-02-07 13:47:41 -0500

    added prettier to clean up code

--- takeNotes-src/deleteUser/app.js
+++ takeNotes-src/deleteUser/app.js
@@ -1,6 +1,10 @@
 // default imports
-const AWS = require('aws-sdk');
+const AWS = require("aws-sdk");
 const DDB = new AWS.DynamoDB({ apiVersion: "2012-10-08" });
+const {
+  CognitoIdentityProviderClient,
+  DeleteUserCommand,
+} = require("@aws-sdk/client-cognito-identity-provider");
 
 // environment variables
 const { TABLE_NAME, ENDPOINT_OVERRIDE, REGION } = process.env;
@@ -8,43 +12,68 @@ const options = { region: REGION };
 AWS.config.update({ region: REGION });
 
 if (ENDPOINT_OVERRIDE !== "") {
-    options.endpoint = ENDPOINT_OVERRIDE;
+  options.endpoint = ENDPOINT_OVERRIDE;
 }
 
 const docClient = new AWS.DynamoDB.DocumentClient(options);
+const cognitoClient = new CognitoIdentityProviderClient({
+  region: "us-east-1",
+});
+
 // response helper
 const response = (statusCode, body, additionalHeaders) => ({
-    statusCode,
-    body: JSON.stringify(body),
-    headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*', ...additionalHeaders },
+  statusCode,
+  body: JSON.stringify(body),
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+    ...additionalHeaders,
+  },
 });
 
 function isValidRequest(context, event) {
-    return (event !== null) &&
-        (event.pathParameters !== null) &&
-        (event.pathParameters.id !== null);
+  return (
+    event !== null &&
+    event.pathParameters !== null &&
+    event.pathParameters.id !== null
+  );
 }
 
 function deleteRecordById(recordId) {
-    let params = {
-        TableName: TABLE_NAME,
-        Key: {
-            id: recordId
-        }
-    };
-    return docClient.delete(params);
+  let params = {
+    TableName: TABLE_NAME,
+    Key: {
+      id: recordId,
+    },
+  };
+  return docClient.delete(params);
+}
+
+function setUpParamas(body) {
+  let body = JSON.parse(body);
+  let params = { AccessToken: body.token };
+  console.log("body: " + body + " params: " + params);
+  return params;
+}
+
+function deleteCognitoUser(body) {
+  const command = new DeleteUserCommand(setUpParamas(body));
+  console.log(command);
+  return cognitoClient.send(command);
 }
 
 // Lambda Handler
 exports.deleteUser = async (event, context, callback) => {
-    if (!isValidRequest(context, event)) {
-        return response(400, { message: "Error: Invalid request" });
-    }
+  if (!isValidRequest(context, event)) {
+    return response(400, { message: "Error: Invalid request" });
+  }
 
-    try {
-        let data = await deleteRecordById(event.pathParameters.id).promise();
-        return response(200, data);
-    } catch (err) {
-        return response(400, { message: err.message });
-    }
-}
+  try {
+    console.log(event);
+    let data = await deleteRecordById(event.pathParameters.id).promise();
+    data = await deleteCognitoUser(event.body);
+    return response(200, data);
+  } catch (err) {
+    return response(400, { message: err.message });
+  }
+};
--- takeNotes-src/putUser/app.js
+++ takeNotes-src/putUser/app.js
@@ -1,5 +1,5 @@
 // default imports
-const AWS = require('aws-sdk');
+const AWS = require("aws-sdk");
 const DDB = new AWS.DynamoDB({ apiVersion: "2012-10-08" });
 
 // environment variables
@@ -8,59 +8,66 @@ const options = { region: REGION };
 AWS.config.update({ region: REGION });
 
 if (ENDPOINT_OVERRIDE !== "") {
-    options.endpoint = ENDPOINT_OVERRIDE;
+  options.endpoint = ENDPOINT_OVERRIDE;
 }
 
 const docClient = new AWS.DynamoDB.DocumentClient(options);
 
 // response helper
 const response = (statusCode, body, additionalHeaders) => ({
-    statusCode,
-    body: JSON.stringify(body),
-    headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*', ...additionalHeaders },
+  statusCode,
+  body: JSON.stringify(body),
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+    ...additionalHeaders,
+  },
 });
 
 function isValidRequest(context, event) {
-    let isIdValid = (event !== null) &&
-        (event.pathParameters !== null) &&
-        (event.pathParameters.id !== null);
+  let isIdValid =
+    event !== null &&
+    event.pathParameters !== null &&
+    event.pathParameters.id !== null;
 
-    let body = event.body;
-    let isBodyValid = (body !== null) &&
-        (body.docBody !== null);
+  let body = event.body;
+  let isBodyValid = body !== null && body.docBody !== null;
 
-    return isIdValid && isBodyValid;
+  return isIdValid && isBodyValid;
 }
 
 function updateRecord(recordId, eventBody) {
-    let d = new Date();
-    eventBody.docBody.id = recordId;    // Preserve ID
-    const params = {
-        TableName: TABLE_NAME,
-        Key: {
-            "id": recordId
-        },
-        UpdateExpression: "set updated = :u, docBody = :d",
-        ExpressionAttributeValues: {
-            ':u': d.toISOString(),
-            ':d': eventBody.docBody
-        },
-        ReturnValues: "ALL_NEW"
-    };
+  let d = new Date();
+  eventBody.docBody.id = recordId; // Preserve ID
+  const params = {
+    TableName: TABLE_NAME,
+    Key: {
+      id: recordId,
+    },
+    UpdateExpression: "set updated = :u, docBody = :d",
+    ExpressionAttributeValues: {
+      ":u": d.toISOString(),
+      ":d": eventBody.docBody,
+    },
+    ReturnValues: "ALL_NEW",
+  };
 
-    return docClient.update(params);
+  return docClient.update(params);
 }
 
 // Lambda Handler
 exports.putUser = async (event, context, callback) => {
-    if (!isValidRequest(context, event)) {
-        return response(400, { message: "Error: Invalid request" });
-    }
+  if (!isValidRequest(context, event)) {
+    return response(400, { message: "Error: Invalid request" });
+  }
 
-    try {
-        let data = await updateRecord(event.pathParameters.id, JSON.parse(event.body)).promise();
-        return response(200, data);
-    } catch (err) {
-        return response(400, { message: err.message });
-    }
-}
+  try {
+    let data = await updateRecord(
+      event.pathParameters.id,
+      JSON.parse(event.body)
+    ).promise();
+    return response(200, data);
+  } catch (err) {
+    return response(400, { message: err.message });
+  }
+};
