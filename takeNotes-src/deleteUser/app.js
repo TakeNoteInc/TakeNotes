e091489fd75f7262344111c5d63054538cb07801
KrishnaSolo <krishna.solanki@mail.utoronto.ca>
2021-02-07 12:47:54 -0500

    Refactored delete user code

--- takeNotes-src/deleteUser/app.js
+++ takeNotes-src/deleteUser/app.js
@@ -1,66 +1,79 @@
 // default imports
-const AWS = require('aws-sdk');
+const AWS = require("aws-sdk");
 const DDB = new AWS.DynamoDB({ apiVersion: "2012-10-08" });
 const {
-    CognitoIdentityProviderClient,
-    DeleteUserCommand,
-  } = require("@aws-sdk/client-cognito-identity-provider");
+  CognitoIdentityProviderClient,
+  DeleteUserCommand,
+} = require("@aws-sdk/client-cognito-identity-provider");
 
 // environment variables
-let { TABLE_NAME, ENDPOINT_OVERRIDE, REGION, POOL_ID } = process.env;
-REGION = 'us-east-1'
+const { TABLE_NAME, ENDPOINT_OVERRIDE, REGION } = process.env;
 const options = { region: REGION };
 AWS.config.update({ region: REGION });
 
 if (ENDPOINT_OVERRIDE !== "") {
-    options.endpoint = ENDPOINT_OVERRIDE;
+  options.endpoint = ENDPOINT_OVERRIDE;
 }
 
 const docClient = new AWS.DynamoDB.DocumentClient(options);
-const cognitoClient = new CognitoIdentityProviderClient({region: "us-east-1"});
+const cognitoClient = new CognitoIdentityProviderClient({
+  region: "us-east-1",
+});
 
 // response helper
 const response = (statusCode, body, additionalHeaders) => ({
-    statusCode,
-    body: JSON.stringify(body),
-    headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*', ...additionalHeaders },
+  statusCode,
+  body: JSON.stringify(body),
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+    ...additionalHeaders,
+  },
 });
 
 function isValidRequest(context, event) {
-    return (event !== null) &&
-        (event.pathParameters !== null) &&
-        (event.pathParameters.id !== null);
+  return (
+    event !== null &&
+    event.pathParameters !== null &&
+    event.pathParameters.id !== null
+  );
 }
 
 function deleteRecordById(recordId) {
-    let params = {
-        TableName: TABLE_NAME,
-        Key: {
-            id: recordId
-        }
-    };
-    return docClient.delete(params);
+  let params = {
+    TableName: TABLE_NAME,
+    Key: {
+      id: recordId,
+    },
+  };
+  return docClient.delete(params);
+}
+
+function setUpParamas(body) {
+  let body = JSON.parse(body);
+  let params = { AccessToken: body.token };
+  console.log("body: " + body + " params: " + params);
+  return params;
+}
+
+function deleteCognitoUser(body) {
+  const command = new DeleteUserCommand(setUpParamas(body));
+  console.log(command);
+  return cognitoClient.send(command);
 }
 
 // Lambda Handler
 exports.deleteUser = async (event, context, callback) => {
-    if (!isValidRequest(context, event)) {
-        return response(400, { message: "Error: Invalid request" });
-    }
+  if (!isValidRequest(context, event)) {
+    return response(400, { message: "Error: Invalid request" });
+  }
 
-    try {
-        console.log(event);
-        let data = await deleteRecordById(event.pathParameters.id).promise();
-        let body = JSON.parse(event.body);
-        console.log(body);
-        let params = { AccessToken: body.token }//UserPoolId: POOL_ID, Username: event.username }
-        console.log('db delete complete = now doing user pool delete')
-        console.log(params);
-        const command = new DeleteUserCommand(params);
-        console.log(command);
-        data = await cognitoClient.send(command)
-        return response(200, data);
-    } catch (err) {
-        return response(400, { message: err.message });
-    }
-}
+  try {
+    console.log(event);
+    let data = await deleteRecordById(event.pathParameters.id).promise();
+    data = await deleteCognitoUser(event.body);
+    return response(200, data);
+  } catch (err) {
+    return response(400, { message: err.message });
+  }
+};
